d8a869961b58110b2958253b1c6161637cfdfe35
Kauan <kauanmoreira96@hotmail.com>
2020-08-11 13:25:12 -0300

    Adicionando o Redux ao projeto

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { Store } from './store';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
+
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider store={Store}>
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
---
+++ src/reducers/clickReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    newValue: ''
+  };
+  export const clickReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case 'CLICK_UPDATE_VALUE':
+        return {
+          ...state,
+          newValue: action.newValue
+        };
+      default:
+        return state;
+    }
+  };
